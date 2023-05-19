05babdd2832761aa1c346835d85ccffee35d717e
Turner Naef <Turner.Naef@gmail.com>
2023-05-19 12:00:51 -0400

    Spring security config

---
+++ src/main/java/com/example/EnergeticYes/config/SecurityConfig.java
@@ -0,0 +1,38 @@
+package com.example.EnergeticYes.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http
+            .cors(Customizer.withDefaults()) // by default uses a Bean by the name of corsConfigurationSource
+            .build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(List.of(("*")));
+        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedHeaders(List.of("*"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+}
--- src/main/java/com/example/EnergeticYes/config/WebMvcConfig.java
+++ src/main/java/com/example/EnergeticYes/config/WebMvcConfig.java
@@ -1,17 +1,19 @@
-package com.example.EnergeticYes.config;
+//package com.example.EnergeticYes.config;
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//
+//@Configuration
+//public class WebMvcConfig implements WebMvcConfigurer {
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/**")
+//                        .allowedMethods("*")
+//                        .allowedOrigins("*")
+//                        .allowedHeaders("*")
+//                        .allowedOriginPatterns("*");
+//    }
+//}
 
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-@Configuration
-public class WebMvcConfig implements WebMvcConfigurer {
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**")
-                        .allowedMethods("*")
-                        .allowedOrigins("*")
-                        .allowedHeaders("*")
-                        .allowedOriginPatterns("*");
-    }
-}
+// ** Ruled unnecessary by usage of SecurityConfig.java **
